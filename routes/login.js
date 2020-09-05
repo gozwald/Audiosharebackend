79e80fe48e5230b9287550911415d2e3d833bdac
Samuel Greenwald <gozwald@gmail.com>
2020-09-05 17:30:08 +0200

    JWT auth implemented

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,18 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const audioShareUser = require("../audioShareUser");
+const jwt = require("jsonwebtoken");
 
 router.post("/", function (req, res, next) {
   const { username, password } = req.body;
-
   audioShareUser
     .findOne({ username }, "password")
     .then((doc) => {
-      if (doc.password === password) res.send("logged in");
-      else res.send("wrong password");
+      if (doc.password === password) {
+        jwt.sign({ username }, "bleeeblaaablooo", function (err, token) {
+          res.cookie("token", token).send("cookie set and logged in");
+        });
+      } else res.send("wrong password");
     })
     .catch((err) => {
       console.error(err);
