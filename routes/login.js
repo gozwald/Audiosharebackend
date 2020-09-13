ae7344a4b5084657f788f8e41f4e79e960ebb669
Samuel Greenwald <gozwald@gmail.com>
2020-09-13 13:05:54 +0200

    login working with front-end

--- routes/login.js
+++ routes/login.js
@@ -6,14 +6,13 @@ const bcrypt = require("bcrypt");
 
 router.post("/", function (req, res, next) {
   const { username, password } = req.body;
-
   audioShareUser
     .findOne({ username }, "password")
     .then((doc) => {
       bcrypt.compare(password, doc.password, (err, result) => {
         if (result === true) {
           jwt.sign({ username }, "bleeeblaaablooo", function (err, token) {
-            res.cookie("token", token).json("cookie set and logged in");
+            res.status(200).json(token);
           });
         } else res.json("wrong password");
       });
