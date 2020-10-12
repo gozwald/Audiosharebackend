09a3f982eb40b4909422600699583f714eec27cd
Samuel Greenwald <gozwald@gmail.com>
2020-10-12 22:13:03 +0200

    fixed typo

--- routes/chatPost.js
+++ routes/chatPost.js
@@ -18,7 +18,7 @@ router.put("/", function (req, res, next) {
 
     post
       .save()
-      .then((e) => res.json(e), io.emit(id, { message, username }))
+      .then((e) => res.json(e), io.emit(id, { message, email }))
       .catch((error) => {
         console.error(error), res.json("something is missing...");
       });
