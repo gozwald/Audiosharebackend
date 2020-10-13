86b5895d1168dcbc2733ae965602833021c7eb97
Samuel Greenwald <gozwald@gmail.com>
2020-10-13 12:32:22 +0200

    all working except for old username posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+// const audioShareAudio = require("../models/audioShareAudio");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// audioShareAudio.audioPost
+//     .find({_id: "5f66739d8e25900017760503"})
+//     .then((result) => {
+//       result.forEach(init => init.chats.forEach(e => {
+//         e['email'] = e['username'];
+//         init.save(function (err) {
+//           if(err) {
+//               console.error(err);
+//           }
+//       });
+//       }))
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
 
 module.exports = router;
