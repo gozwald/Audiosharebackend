d9494c96ce0380eee9151ac134b4bc403b2153aa
Samuel Greenwald <gozwald@gmail.com>
2020-10-23 16:39:27 +0200

    cleaned async func

--- routes/chatPost.js
+++ routes/chatPost.js
@@ -4,30 +4,26 @@ const audioSharePost = require("../models/audioShareAudio");
 var socketApi = require("../socketAPI");
 var io = socketApi.io;
 
-router.put("/", function (req, res, next) {
+router.put("/", async function (req, res, next) {
   const { _id } = req.decoded;
   const { id, message } = req.body;
 
-  const runCode = async () => {
-    const post = await audioSharePost.audioPost.findOne({ _id: id });
+  const post = await audioSharePost.audioPost.findOne({ _id: id });
 
-    post.chats.push({
-      message: message,
-      user: _id,
+  post.chats.push({
+    message: message,
+    user: _id,
+  });
+
+  post
+    .save()
+    .then((e) => {
+      res.json(e);
+      io.emit(id, { message, _id });
+    })
+    .catch((error) => {
+      console.error(error), res.json("something is missing...");
     });
-
-    post
-      .save()
-      .then((e) => {
-        res.json(e);
-        io.emit(id, { message, _id });
-      })
-      .catch((error) => {
-        console.error(error), res.json("something is missing...");
-      });
-  };
-
-  runCode();
 });
 
 module.exports = router;
