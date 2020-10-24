2846ef1fa9c92ba0b3cdd50c8665fcef5b7fa5d1
Samuel Greenwald <gozwald@gmail.com>
2020-10-24 19:13:51 +0200

    small backend tweak

--- routes/login.js
+++ routes/login.js
@@ -9,17 +9,21 @@ router.post("/", function (req, res, next) {
   audioSharePost.users
     .findOne({ email }, "password")
     .then((doc) => {
-      bcrypt.compare(password, doc.password, (err, result) => {
-        if (result === true) {
-          jwt.sign({ email, _id: doc._id }, process.env.JWT_CRED, function (
-            err,
-            token
-          ) {
-            console.log("user logged in");
-            res.status(200).json(token);
-          });
-        } else res.sendStatus(401);
-      });
+      if (!doc) {
+        res.sendStatus(401);
+      } else {
+        bcrypt.compare(password, doc.password, (err, result) => {
+          if (result === true) {
+            jwt.sign({ email, _id: doc._id }, process.env.JWT_CRED, function (
+              err,
+              token
+            ) {
+              console.log("user logged in");
+              res.status(200).json(token);
+            });
+          }
+        });
+      }
     })
     .catch((err) => {
       res.sendStatus(500);
--- routes/register.js
+++ routes/register.js
@@ -16,8 +16,17 @@ router.post("/", function (req, res, next) {
 
     newUser.save(function (error, document) {
       if (error)
-        console.error(error), res.status(400).json("something went wrong...");
-      else console.log(document), res.status(200).json(document);
+        console.error(error),
+          res
+            .status(400)
+            .json("Sorry! This email has already been registered!");
+      else
+        console.log(document),
+          res
+            .status(200)
+            .json(
+              "Welcome aboard " + document.first + "! Click below to login!"
+            );
     });
   });
 });
