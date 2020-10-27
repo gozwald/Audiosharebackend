e80298c875991aa54d138622cbe5367376bda52f
Samuel Greenwald <gozwald@gmail.com>
2020-10-27 10:04:28 +0100

    refactor react module

--- routes/reactPost.js
+++ routes/reactPost.js
@@ -9,35 +9,26 @@ router.put("/", async (req, res, next) => {
     const { _id: mongouserid } = req.decoded;
     const { id } = req.body;
 
-    const post = await audioSharePost.audioPost.findOne({ _id: id });
+    let find = await audioSharePost.audioPost
+      .findOne({ _id: id })
+      .select({ "react._id": mongouserid });
 
-    if (
-      !post.react.find((e) => toString(e.user._id) === toString(mongouserid))
-    ) {
-      post.react.push({
+    if (!find.react.length) {
+      find = await audioSharePost.audioPost.findOne({ _id: id });
+      find.react.push({
         user: mongouserid,
       });
-      const save = await post.save();
-      const populated = await save.execPopulate("user chats.user react.user");
-      io.emit(id, populated);
-      res.sendStatus(200);
     } else {
-      const find = await audioSharePost.audioPost
-        .findOne({ _id: id })
-        .select({ "react._id": mongouserid });
-
-      const update = await audioSharePost.audioPost.findOneAndUpdate(
+      find = await audioSharePost.audioPost.findOneAndUpdate(
         { _id: id },
         { $pull: { react: { _id: find.react[0]._id } } },
         { new: true }
       );
-
-      const save = await update.save();
-      const populated = await save.execPopulate("user chats.user react.user");
-
-      io.emit(id, populated);
-      res.sendStatus(200);
     }
+    const save = await find.save();
+    const populated = await save.execPopulate("user chats.user react.user");
+    io.emit(id, populated);
+    res.sendStatus(200);
   } catch (error) {
     console.log(error);
   }
