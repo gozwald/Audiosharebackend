6d843453acc84d34860d86137ebb24705aae2865
Samuel Greenwald <gozwald@gmail.com>
2020-10-27 09:24:10 +0100

    like function working

--- routes/reactPost.js
+++ routes/reactPost.js
@@ -11,13 +11,33 @@ router.put("/", async (req, res, next) => {
 
     const post = await audioSharePost.audioPost.findOne({ _id: id });
 
-    post.react.push({
-      user: mongouserid,
-    });
-    const save = await post.save();
-    const populated = await save.execPopulate("user chats.user react.user");
-    io.emit(id, populated);
-    res.sendStatus(200);
+    if (
+      !post.react.find((e) => toString(e.user._id) === toString(mongouserid))
+    ) {
+      post.react.push({
+        user: mongouserid,
+      });
+      const save = await post.save();
+      const populated = await save.execPopulate("user chats.user react.user");
+      io.emit(id, populated);
+      res.sendStatus(200);
+    } else {
+      const find = await audioSharePost.audioPost
+        .findOne({ _id: id })
+        .select({ "react._id": mongouserid });
+
+      const update = await audioSharePost.audioPost.findOneAndUpdate(
+        { _id: id },
+        { $pull: { react: { _id: find.react[0]._id } } },
+        { new: true }
+      );
+
+      const save = await update.save();
+      const populated = await save.execPopulate("user chats.user react.user");
+
+      io.emit(id, populated);
+      res.sendStatus(200);
+    }
   } catch (error) {
     console.log(error);
   }
